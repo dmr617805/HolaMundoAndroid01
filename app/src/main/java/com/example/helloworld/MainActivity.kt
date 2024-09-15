8d9f958f257675419dd3b1cca04bc71070fd3f4b
dmr617805 <157419308+dmr617805@users.noreply.github.com>
2024-09-14 18:12:59 -0600

    version inicial
    version inicial

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "David",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(){
+        Text(
+            text = "Hola $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hola $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hola $name!",
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("David")
+    }
+}
